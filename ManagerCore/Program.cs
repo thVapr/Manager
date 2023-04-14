bdf3734e7dd8ab50b91ee6fd7583f921a57c8bae
V Vapr <vaprmail@gmail.com>
2023-04-14 11:18:29 +0400

    Add auth template

--- ManagerCore/Program.cs
+++ ManagerCore/Program.cs
@@ -3,11 +3,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddCors(options => options.AddPolicy("CorsPolicy",
-    builder =>
-    {
-        builder.WithOrigins("http://localhost:4200");
-    }
-    ));
+builder =>
+{
+    builder.WithOrigins("http://localhost:4200");
+}
+));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
