479377659b278d7def120c2a78dee13c873901d3
V Vapr <vaprmail@gmail.com>
2023-05-17 08:56:37 +0400

    Add private data constants

---
+++ ManagerData/Constants/DataConstants.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ManagerData.Constants;
+
+public class DataConstants
+{
+    public const string AuthConnectionString = 
+        "Host=localhost;Port=5432;Database=manager_auth;Username=postgres;Password=password";
+
+    public const string ManagerConnectionString =
+        "Host=localhost;Port=5432;Database=manager_data;Username=postgres;Password=password";
+}
