f001188d21e1c48478e99470229456ab768264f5
Vapr <vaprmail@gmail.com>
2025-05-31 16:39:29 +0400

    Refactor: delete unused constants

--- ManagerLogic/PublicConstants.cs
+++ ManagerLogic/PublicConstants.cs
@@ -6,11 +6,4 @@ public abstract class PublicConstants
     public const string Id = "id";
     public const string Email = "email";
     public const string Role = "role";
-
-    public enum Task
-    {
-        Todo,
-        Doing,
-        Done
-    }
 }
