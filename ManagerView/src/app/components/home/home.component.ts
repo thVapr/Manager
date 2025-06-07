e7b7974d5a91f1ba6b145b0fb9709f941ce27595
Vapr <vaprmail@gmail.com>
2025-06-07 18:21:07 +0400

    Feat: add type stats bar

--- ManagerView/src/app/components/home/home.component.ts
+++ ManagerView/src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { PartService } from 'src/app/services/part/part.service';
 import { TaskService } from 'src/app/services/task/task.service';
+import { PartTaskType } from '../models/part-task-type';
 
 @Component({
     selector: 'app-home',
@@ -9,10 +10,13 @@ import { TaskService } from 'src/app/services/task/task.service';
     standalone: false
 })
 export class HomeComponent implements OnInit {
-    basicData: any;
-    basicOptions: any;
-    statusCounts: StatusCount[] = [];
-    progress: number = 0;
+    basicData : any;
+    basicOptions : any;
+    statusCounts : StatusCount[] = [];
+    progress : number = 0;
+    types : StatusCount[] = [];
+    typeData : any;
+    typeOptions : any;
 
     constructor(private cd: ChangeDetectorRef,
                 private partService: PartService,
@@ -31,16 +35,33 @@ export class HomeComponent implements OnInit {
                 color: this.getHeaderColor(status.globalStatus!)
               })).sort((a,b)=> a.statusOrder! - b.statusOrder!);
               const completedTasks : number = this.statusCounts.find(status => status.statusOrder === 110)?.taskCount!;
-              
               this.progress = Math.round(completedTasks / tasks.length * 100);
-              this.initChart();
+              this.partService.getTypesById(this.partService.getPartId()!).subscribe(
+                (types) => {
+                  this.types = types.map(type => ({
+                    statusId: type.id,
+                    statusName: type.name,
+                    statusOrder: null!,
+                    taskCount: tasks.filter(task => task.taskTypeId === type.id).length!,
+                    color: null!
+                  }));
+                  this.types.push({
+                    statusId: null!,
+                    statusName: "Не определены",
+                    statusOrder: null!,
+                    taskCount: tasks.filter(task => task.taskTypeId === null).length!,
+                    color: null!
+                  })
+                  this.initCharts();
+                }
+              );
             }
           });
         }
       });
     }
 
-    private initChart() {
+    private initCharts() {
       const documentStyle = getComputedStyle(document.documentElement);
       const textColor = documentStyle.getPropertyValue('--p-text-color');
       const textColorSecondary = documentStyle.getPropertyValue('--p-text-muted-color');
@@ -56,7 +77,18 @@ export class HomeComponent implements OnInit {
           }
         ],
       };
+      this.typeData = {
+        labels: [...this.types.map(status => status.statusName)],
+        datasets: [
+          {
+            label: 'Типы',
+            data: [...this.types.map(status => status.taskCount)],
+          }
+        ],
+      };
       this.basicOptions = {
+        responsive: true,
+        maintainAspectRatio: true,
         plugins: {
           legend: {
             labels: {
@@ -83,6 +115,24 @@ export class HomeComponent implements OnInit {
             },
           },
         },
+      };
+      this.typeOptions = {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+              labels: {
+                  color: textColor
+              }
+            }
+        },
+        scales: {
+          r: {
+            grid: {
+                color: surfaceBorder
+            }
+          }
+        }
       };
       this.cd.markForCheck()
     } 
