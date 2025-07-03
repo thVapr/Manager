2507f4cdcce95883c6ff2aa28d0ac3e35869968d
Vapr <vaprmail@gmail.com>
2025-07-03 17:18:42 +0400

    Fix: elimination syntax consequences after merge conflict

--- ManagerCore/Models/PrivilegeChangeRequest.cs
+++ ManagerCore/Models/PrivilegeChangeRequest.cs
@@ -1,9 +1,6 @@
-﻿using ManagerLogic.Models;
-using System.ComponentModel.DataAnnotations;
+﻿namespace ManagerCore.Models;
 
-namespace ManagerCore.Models;
-
-public class PrivilegeChangeRequest : IPartAllocationModel
+public class PrivilegeChangeRequest
 {
     public int Privilege { get; set; }
 }
